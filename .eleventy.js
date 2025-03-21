278740209432fe79b76650b2d9bfa86e70de7911
Shnhlms <nedalturas527@gmail.com>
2025-03-22 00:41:05 +0800

    changed the current configuration:

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,13 @@
-module.exports = function(eleventyConfig) {
-  // Copy these directories as-is to the output folder
-  eleventyConfig.addPassthroughCopy("src/assets");
-  
-  // Set custom directories for input, output, includes, and data
-  return {
-    dir: {
+module.exports = function(eleventyConfig){
+
+  return{
+
+    dir:{
+
       input: "src",
-      output: "_site",
-      includes: "../_includes",
-      data: "../_data"
+      output: "dist",
     },
-    templateFormats: ["njk", "md", "html"],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+
   };
+
 };
