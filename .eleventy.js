e1f8c1793d97a89308d2b587c6a0cbac4b93adfa
Ned <nedalturas527@gmail.com>
2025-03-29 08:51:34 +0400

    fixed the tools not working, instead of putting the js per component, it has their own script in the js folder now. Name convention should be the same as the name of the component file

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function(eleventyConfig){
-
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
   return{
 
     dir:{
-
       input: "src",
       output: "dist",
     },
---
+++ dist/js/formatter.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.ui.dropdown').dropdown();
+});
+
+  function showValues() {
+  const refNum = document.getElementById('reference_number').value;
+  const fullName = document.getElementById('full_name').value;
+  const phoneNum = document.getElementById('phone_number').value;
+  const concernSelect = document.getElementById('concern_select').value;
+
+  console.log('Ref Number:', refNum);
+  console.log('Full Name:', fullName);
+  console.log('Phone Number:', phoneNum);
+  console.log('Concern:', concernSelect);
+
+  // Display values in the UI
+  document.getElementById('ref_value').innerText = `Reference Number: ${refNum}`;
+
+  document.getElementById('display_value').innerText =
+    `Full Name: ${fullName}\nPhone Number: ${phoneNum}\nConcern: ${concernSelect}`;
+}
+
+function clearForm() {
+  document.getElementById('ref_value').reset($);
+  document.getElementById('display_value').innerText = '';
+}
+
