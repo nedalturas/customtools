885b515e400a4dc0865bbb6dcf0c82243f8b92ce
Ned <nedalturas527@gmail.com>
2026-02-28 03:14:27 +0800

    change: removed reference number field as it was not needed anymore

--- src/js/formatter.js
+++ src/js/formatter.js
@@ -1,13 +1,7 @@
 function showValues() {
-  const refNum = document.getElementById('reference_number').value;
   const fullName = document.getElementById('full_name').value;
   const phoneNum = document.getElementById('phone_number').value;
   const concernSelect = document.getElementById('concern_select').value;
-
-  const refValueElement = document.getElementById('ref_value');
-  if (refValueElement) {
-    refValueElement.innerHTML = `Reference: ${refNum}`;
-  }
 
   const displayValueElement = document.getElementById('display_value');
   if (displayValueElement) {
@@ -21,11 +15,6 @@ function clearConverterForm() {
   const form = document.getElementById('template_form');
   if (form) {
     form.reset();
-  }
-
-  const refValueElement = document.getElementById('ref_value');
-  if (refValueElement) {
-    refValueElement.innerHTML = 'Reference: '; // or clear it completely
   }
 
   const displayValueElement = document.getElementById('display_value');
