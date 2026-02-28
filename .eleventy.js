f112b035e38bfbc4021aed71d388e9ded6930a64
Ned <nedalturas527@gmail.com>
2026-02-28 11:38:21 +0400

    static

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return{
 
