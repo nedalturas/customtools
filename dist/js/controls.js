f239e790372f23419660bcbe9fb1577d3d198702
httpwanderer <nedalturas527@gmail.com>
2026-02-28 14:14:03 +0400

    Updated controls.njk

--- dist/js/controls.js
+++ dist/js/controls.js
@@ -1,11 +1,17 @@
-fetch('/assets/data/concern_codes.json')
-  .then(res => res.json())
-  .then(data => {
-    const select = document.getElementById('concern-codes');
+$(document).ready(function() {
+  fetch('/assets/data/concern_codes.json')
+    .then(res => res.json())
+    .then(data => {
+      const select = document.getElementById('concern-codes');
 
-    data.forEach(({ id, label }) => {
-      select.innerHTML += `<option value="${id}">${label}</option>`;
+      data.forEach(({ id, label }) => {
+        select.innerHTML += `<option value="${id}">${label}</option>`;
+      });
+
+      // Init Fomantic dropdown after options are populated
+      $('#concern-codes').dropdown();
+    })
+    .catch(error => {
+      console.error('Error loading concern codes:', error);
     });
-
-    // Init Fomantic dropdown after options are populated
-  });
+});
