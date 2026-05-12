0723df48d0b987db987a82f2453b6e1d1e4cbc5b
nedalturas <nedalturas527@gmail.com>
2026-05-13 04:37:31 +0800

    testing dropdown fetch

--- dist/assets/js/vip_tagging.js
+++ dist/assets/js/vip_tagging.js
@@ -1,7 +1,9 @@
-document.getElementById('concern_select').addEventListener('change', (e) => {
-    const selected = vipTags.find(s => s.name === e.target.value);
-    const list = document.getElementById('managers-list');
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("concern_select").addEventListener("change", (e) => {
+    const selected = vip_tags.find((s) => s.name === e.target.value);
+    const list = document.getElementById("managers-list");
     list.innerHTML = selected
-        ? selected.managers.map(m => `<li>${m.name}</li>`).join('')
-        : '';
+      ? selected.managers.map((m) => `<li>${m.name}</li>`).join("")
+      : "";
+  });
 });
