9208fd80abfc56fa48d37b9e245a025ac0b2c274
Ned <nedalturas527@gmail.com>
2026-05-16 12:28:45 +0400

    removed unused js files, removed compensator tab for now

--- dist/assets/js/semantic_init.js
+++ dist/assets/js/semantic_init.js
@@ -1,7 +1,5 @@
-$(document).ready(function () {
-    $('.ui.dropdown').dropdown()
-    $('.menu .item').tab();
-    $('#date_calendar').calendar({ type: 'date' });
-    $('#time_calendar').calendar({ type: 'time' });
-    $('.ui.sticky').sticky();
-});
+$('.ui.dropdown').dropdown()
+$('.menu .item').tab();
+$('#date_calendar').calendar({ type: 'date' });
+$('#time_calendar').calendar({ type: 'time' });
+$('.ui.sticky').sticky();
